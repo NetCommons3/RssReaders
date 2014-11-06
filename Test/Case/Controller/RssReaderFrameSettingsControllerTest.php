339c763d0741ddd4fcee1d1e94fcfb703f7815ef
almiura <alpaca0121@gmail.com>
2014-11-06 16:37:43 +0900

    テストケースを追加

--- Test/Case/Controller/RssReaderFrameSettingsControllerTest.php
+++ Test/Case/Controller/RssReaderFrameSettingsControllerTest.php
@@ -92,11 +92,117 @@ class RssReaderFrameSettingsControllerTest extends ControllerTestCase {
 				'display_summary' => true
 			)
 		);
-		$result = $this->testAction('/rss_readers/rss_reader_frame_settings/edit', array('method' => 'post', 'data' => $data));
+		$frameId = 1;
+
+		$result = $this->testAction(
+			'/rss_readers/rss_reader_frame_settings/edit/' . $frameId,
+			array('method' => 'post', 'data' => $data)
+		);
 		$this->assertTextContains($data['RssReaderFrameSetting']['frame_key'], $result);
 		$this->assertTextContains($data['RssReaderFrameSetting']['display_number_per_page'], $result);
 		$this->assertTextContains($data['RssReaderFrameSetting']['display_site_info'], $result);
 		$this->assertTextContains($data['RssReaderFrameSetting']['display_summary'], $result);
+	}
+
+/**
+ * test edit case not room role
+ *
+ * @author Kosuke Miura <k_miura@zenk.co.jp>
+ * @return void
+ */
+	public function testEditNotRoomRole() {
+		CakeSession::write('Auth.User', null);
+		$user = array(
+			'id' => 999
+		);
+		CakeSession::write('Auth.User', $user);
+		$frameId = 1;
+
+		$data = array(
+			'RssReaderFrameSetting' => array(
+				'frame_key' => 'jidkgji409u490u032jalk4j02jgraljijgkak494958030jj0hjaalek339rr',
+				'display_number_per_page' => 10,
+				'display_site_info' => true,
+				'display_summary' => true
+			)
+		);
+		try {
+			$this->testAction(
+				'/rss_readers/rss_reader_frame_settings/edit/' . $frameId,
+				array('method' => 'post', 'data' => $data)
+			);
+		} catch (ForbiddenException $e) {
+			$this->assertEquals('Forbidden', $e->getMessage());
+		}
+	}
+
+/**
+ * test getEditToken
+ *
+ * @author Kosuke Miura <k_miura@zenk.co.jp>
+ * @return void
+ */
+	public function testGetEditToken() {
+		$frameId = 1;
+		$this->testAction(
+			'/rss_readers/rss_reader_frame_settings/getEditToken/' . $frameId . '/',
+			array('method' => 'get')
+		);
+		$this->assertTextContains('data[RssReaderFrameSetting][frame_key]', $this->view);
+		$this->assertTextContains('data[RssReaderFrameSetting][display_number_per_page]', $this->view);
+		$this->assertTextContains('data[RssReaderFrameSetting][display_site_info]', $this->view);
+		$this->assertTextContains('data[RssReaderFrameSetting][display_summary]', $this->view);
+		$this->assertTextContains('data[RssReaderFrameSetting][id]', $this->view);
+		$this->assertTextContains('data[RssReaderFrameSetting][frame_key]', $this->view);
+	}
+
+/**
+ * test getEditToken case not exist frame
+ *
+ * @author Kosuke Miura <k_miura@zenk.co.jp>
+ * @return void
+ */
+	public function testGetEditTokenNotExistFrame() {
+		$frameId = 999;
+		try {
+			$this->testAction(
+				'/rss_readers/rss_reader_frame_settings/getEditToken/' . $frameId . '/',
+				array('method' => 'get')
+			);
+		} catch (ForbiddenException $e) {
+			$this->assertEquals('NetCommonsFrame', $e->getMessage());
+		}
+	}
+
+/**
+ * test edit case get
+ *
+ * @author Kosuke Miura <k_miura@zenk.co.jp>
+ * @return void
+ */
+	public function testEditGet() {
+		$frameId = 1;
+
+		$this->testAction(
+			'/rss_readers/rss_reader_frame_settings/edit/' . $frameId,
+			array('method' => 'get')
+		);
+		$this->assertTextContains(
+			'data[RssReaderFrameSetting][frame_key]',
+			$this->view
+		);
+		$this->assertTextContains(
+			'data[RssReaderFrameSetting][display_number_per_page]',
+			$this->view
+		);
+		$this->assertTextContains(
+			'data[RssReaderFrameSetting][display_site_info]',
+			$this->view
+		);
+		$this->assertTextContains(
+			'data[RssReaderFrameSetting][display_summary]',
+			$this->view
+		);
 	}
 
 /**
