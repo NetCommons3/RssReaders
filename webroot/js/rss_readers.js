74e657e52a841b6f9fb2577765ffcf684dfd6fec
s-nakajima <nakajimashouhei@gmail.com>
2015-03-21 16:16:12 +0900

    Fix of gjslint error.

--- webroot/js/rss_readers.js
+++ webroot/js/rss_readers.js
@@ -11,61 +11,64 @@
  * @param {string} Controller name
  * @param {function($scope, $sce, NetCommonsTab)} Controller
  */
-NetCommonsApp.controller('RssReaders', function($scope, $window, NetCommonsTab) {
+NetCommonsApp.controller('RssReaders',
+    function($scope, $window, NetCommonsTab) {
 
-  /**
-   * tab
-   *
-   * @type {object}
-   */
-  $scope.tab = NetCommonsTab.new();
+      /**
+       * tab
+       *
+       * @type {object}
+       */
+      $scope.tab = NetCommonsTab.new();
 
-  /**
-   * Initialize
-   *
-   * @param {Object.<string>} rssReaderData
-   * @return {void}
-   */
-  $scope.initialize = function(data) {
-    $scope.frameId = data.frameId;
-  };
+      /**
+       * Initialize
+       *
+       * @param {Object.<string>} rssReaderData
+       * @return {void}
+       */
+      $scope.initialize = function(data) {
+        $scope.frameId = data.frameId;
+      };
 
-  /**
-   * Switching display of item summary
-   *
-   * @return {void}
-   */
-  $scope.switchDisplaySummary = function(id) {
-    var element = $(id);
-    if (element.hasClass('hidden')) {
-      element.removeClass('hidden');
-    } else {
-      element.addClass('hidden');
-    }
-  };
+      /**
+       * Switching display of item summary
+       *
+       * @return {void}
+       */
+      $scope.switchDisplaySummary = function(id) {
+        var element = $(id);
+        if (element.hasClass('hidden')) {
+          element.removeClass('hidden');
+        } else {
+          element.addClass('hidden');
+        }
+      };
 
-  /**
-   * Switching display of the site information
-   *
-   * @return {void}
-   */
-  $scope.switchDisplaySiteInfo = function() {
-    var element = $('#nc-rss-readers-' + $scope.frameId + ' div.rss-site-info');
-    if (element.hasClass('hidden')) {
-      element.removeClass('hidden');
-    } else {
-      element.addClass('hidden');
-    }
-  };
+      /**
+       * Switching display of the site information
+       *
+       * @return {void}
+       */
+      $scope.switchDisplaySiteInfo = function() {
+        var element = $('#nc-rss-readers-' +
+                        $scope.frameId + ' div.rss-site-info');
+        if (element.hasClass('hidden')) {
+          element.removeClass('hidden');
+        } else {
+          element.addClass('hidden');
+        }
+      };
 
-  /**
-   * Get site information
-   *
-   * @return {void}
-   */
-  $scope.getSiteInfo = function() {
-    var element = $('input[name="data[RssReader][url]"]');
-    $window.location.href = '/rss_readers/rss_readers/edit/' + $scope.frameId +
-                    '?url=' + encodeURIComponent(element[0].value);
-  };
-});
+      /**
+       * Get site information
+       *
+       * @return {void}
+       */
+      $scope.getSiteInfo = function() {
+        var element = $('input[name="data[RssReader][url]"]');
+        $window.location.href =
+            '/rss_readers/rss_readers/edit/' + $scope.frameId +
+            '?url=' + encodeURIComponent(element[0].value);
+      };
+    });
