890f63a003f80d516914c5e3cc8d5ba00f02c0e0
s-nakajima <nakajimashouhei@gmail.com>
2015-08-14 14:31:14 +0900

    Fixture共通化による修正

--- Test/Case/Controller/RssReadersControllerTestBase.php
+++ Test/Case/Controller/RssReadersControllerTestBase.php
@@ -33,31 +33,10 @@ class RssReadersControllerTestBase extends YAControllerTestCase {
  * @var array
  */
 	public $fixtures = array(
-		//'plugin.blocks.block',
-		//'plugin.blocks.block_role_permission',
-		//'plugin.boxes.box',
-		//'plugin.boxes.boxes_page',
-		//'plugin.containers.container',
-		//'plugin.containers.containers_page',
 		'plugin.comments.comment',
-		//'plugin.frames.frame',
-		//'plugin.m17n.language',
-		//'plugin.net_commons.site_setting',
-		//'plugin.pages.languages_page',
-		//'plugin.pages.page',
-		//'plugin.pages.space',
-		//'plugin.plugin_manager.plugin',
-		//'plugin.plugin_manager.plugins_room',
-		//'plugin.roles.default_role_permission',
-		//'plugin.rooms.roles_room',
-		//'plugin.rooms.roles_rooms_user',
-		//'plugin.rooms.room',
-		//'plugin.rooms.room_role_permission',
 		'plugin.rss_readers.rss_reader',
 		'plugin.rss_readers.rss_reader_frame_setting',
 		'plugin.rss_readers.rss_reader_item',
-		//'plugin.users.user',
-		//'plugin.users.user_attributes_user',
 	);
 
 /**
