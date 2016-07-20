c15f7ba93fe7e56ece150cf35ecd84be79a1f1b2
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-07-20 19:19:01 +0900

    マイグレーション - rss_reader_settings削除 Fixture追加 - BlockSettingForLinkFixture Fixture削除 - RssReaderSettingFixture

---
+++ Config/Migration/1469009771_delete_rss_reader_settings.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * rss_reader_settingsテーブル削除 Migration
+ *
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+/**
+ * rss_reader_settingsテーブル削除 Migration
+ *
+ * @package NetCommons\RssReaders\Config\Migration
+ */
+class DeleteRssReaderSettings extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'delete_rss_reader_settings';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+			'drop_table' => array(
+				'rss_reader_settings'
+			),
+		),
+		'down' => array(
+			'create_table' => array(
+				'rss_reader_settings' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary', 'comment' => 'ID'),
+					'block_key' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'ブロックキー', 'charset' => 'utf8'),
+					'use_workflow' => array('type' => 'boolean', 'null' => false, 'default' => '1', 'comment' => '承認機能 0:使わない 1:使う'),
+					'created_user' => array('type' => 'integer', 'null' => true, 'default' => '0', 'unsigned' => false, 'comment' => '作成者'),
+					'created' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '作成日時'),
+					'modified_user' => array('type' => 'integer', 'null' => true, 'default' => '0', 'unsigned' => false, 'comment' => '更新者'),
+					'modified' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '更新日時'),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
+				),
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		return true;
+	}
+}
