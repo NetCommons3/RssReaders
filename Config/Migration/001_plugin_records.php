206b9741201d1b40711738a024af78b2a41e37a1
s-nakajima <nakajimashouhei@gmail.com>
2016-11-21 18:14:29 +0900

    Boxの切り替え機能の再修正 https://github.com/NetCommons3/NetCommons3/issues/726

--- Config/Migration/001_plugin_records.php
+++ Config/Migration/001_plugin_records.php
@@ -9,6 +9,7 @@
  */
 
 App::uses('NetCommonsMigration', 'NetCommons.Config/Migration');
+App::uses('Space', 'Rooms.Model');
 
 /**
  * Add plugin migration
@@ -74,14 +75,7 @@ class PluginRecords extends NetCommonsMigration {
 				'plugin_key' => 'rss_readers',
 			)
 		),
-		'PluginsRoom' => array(
-			//パブリックスペース
-			array('room_id' => '2', 'plugin_key' => 'rss_readers', ),
-			//プライベートスペース
-			array('room_id' => '3', 'plugin_key' => 'rss_readers', ),
-			//グループスペース
-			array('room_id' => '4', 'plugin_key' => 'rss_readers', ),
-		),
+		//PluginsRoomは、beforeでセットする
 	);
 
 /**
@@ -91,6 +85,29 @@ class PluginRecords extends NetCommonsMigration {
  * @return bool Should process continue
  */
 	public function before($direction) {
+		$pluginName = $this->records['Plugin'][0]['key'];
+		$this->records['PluginsRoom'] = array(
+			//サイト全体
+			array(
+				'room_id' => Space::getRoomIdRoot(Space::WHOLE_SITE_ID, 'Room'),
+				'plugin_key' => $pluginName
+			),
+			//パブリックスペース
+			array(
+				'room_id' => Space::getRoomIdRoot(Space::PUBLIC_SPACE_ID, 'Room'),
+				'plugin_key' => $pluginName
+			),
+			//プライベートスペース
+			array(
+				'room_id' => Space::getRoomIdRoot(Space::PRIVATE_SPACE_ID, 'Room'),
+				'plugin_key' => $pluginName
+			),
+			//グループスペース
+			array(
+				'room_id' => Space::getRoomIdRoot(Space::COMMUNITY_SPACE_ID, 'Room'),
+				'plugin_key' => $pluginName
+			),
+		);
 		return true;
 	}
 
